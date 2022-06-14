5f1ea1371bd0e66668491aa64cf63ab2e0efda5d
niltrix <axlgilby@gmail.com>
2022-06-15 02:24:00 +0900

    add movement shortcut in insert mode

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -44,6 +44,11 @@ keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
+-- provide hjkl movements in Insert mode via the <Alt> modifier key
+--keymap("i", "<A-h>", "<C-o>h", opts)
+--keymap("i", "<A-j>", "<C-o>j", opts)
+--keymap("i", "<A-k>", "<C-o>k", opts)
+--keymap("i", "<A-l>", "<C-o>l", opts)
 
 -- Visual --
 -- Stay in indent mode
